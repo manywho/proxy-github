2fab33a17b4bf8728550eb055394f7311727e523
Jonjo McKay <jonjo@jonjomckay.com>
2019-06-18 16:33:19 +0100

    Cleaned up a bit

--- src/server.js
+++ src/server.js
@@ -1,40 +1,29 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
+const http = require('http');
+const httpProxy = require('http-proxy');
 
-//
-// Create a proxy server with custom application logic
-//
-var proxy = httpProxy.createProxyServer({
-  changeOrigin: true,
-  secure: true
+const proxy = httpProxy.createProxyServer({
+    changeOrigin: true
 });
 
-// To modify the proxy connection before data is sent, you can listen
-// for the 'proxyReq' event. When the event is fired, you will receive
-// the following arguments:
-// (http.ClientRequest proxyReq, http.IncomingMessage req,
-//  http.ServerResponse res, Object options). This mechanism is useful when
-// you need to modify the proxy request before the proxy connection
-// is made to the target.
-//
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('Authorization', 'Basic ' + process.env["GITHUB_TOKEN"]);
+proxy.on('proxyReq', function (proxyRequest) {
+    proxyRequest.setHeader('Authorization', 'Basic ' + process.env["GITHUB_TOKEN"]);
 });
 
-var server = http.createServer(function(req, res) {
-  console.log(req.url);
+const server = http.createServer(function (req, res) {
+    // Ensure the URL is in our whitelist,
+    if (/\/repos\/manywho\/(.*)\/pulls(.*)$/.test(req.url)) {
+        proxy.web(req, res, {
+            target: 'https://api.github.com'
+        });
+        return;
+    }
 
-  if (/\/repos\/manywho\/(.*)\/pulls(.*)$/.test(req.url)) {
-    proxy.web(req, res, {
-      target: 'https://api.github.com'
-    });
-  } else {
+    // Otherwise, throw an error
     res.writeHead(401);
     res.write("You're not allowed to access that URL");
     res.end();
-  }
 });
 
-console.log("listening on port 5050");
+console.log("Now listening on port 5050");
 
 server.listen(5050);
